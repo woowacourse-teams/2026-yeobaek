7de5ee2ac5d8c94c2d825a4a4bea75fbed5607a9
Eungyeong Jo <joeungyeong23@gmail.com>
2026-08-05 22:53:11 +0900

    refactor: 사용되지 않는 import 정리

--- android/shared/src/commonMain/kotlin/com/yeobaek/core/designsystem/component/YeobaekButton.kt
+++ android/shared/src/commonMain/kotlin/com/yeobaek/core/designsystem/component/YeobaekButton.kt
@@ -9,7 +9,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.yeobaek.core.designsystem.theme.YeobaekTheme
 
 @Composable
