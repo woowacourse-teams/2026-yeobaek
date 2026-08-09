98bc1f84f335f500fa6b212142cfe427d4394a6e
Eungyeong Jo <joeungyeong23@gmail.com>
2026-08-09 20:06:44 +0900

    refactor: mock 데이터를 생성자 기본 인자로 이동

--- android/shared/src/commonMain/kotlin/com/yeobaek/feature/home/ReaderHomeStateHolder.kt
+++ android/shared/src/commonMain/kotlin/com/yeobaek/feature/home/ReaderHomeStateHolder.kt
@@ -16,17 +16,17 @@ class ReaderHomeStateHolder(
 
 @Composable
 fun rememberReaderHomeStateHolder(
-    initialUiState: ReaderHomeUiState = ReaderHomeUiState(),
+    initialUiState: ReaderHomeUiState = ReaderHomeUiState(
+        currentlyReadingBookUiModel = CurrentlyReadingBookUiModel(
+            groupName = "고전 읽는 오후 모임",
+            title = "데미안",
+            coverImageUrl = "https://contents.kyobobook.co.kr/sih/fit-in/400x0/pdt/9791189413408.jpg",
+            authors = "헤르만 헤세",
+            progressRate = 12,
+        ),
+    ),
 ): ReaderHomeStateHolder = remember {
     ReaderHomeStateHolder(
-        initialUiState = ReaderHomeUiState(
-            currentlyReadingBookUiModel = CurrentlyReadingBookUiModel(
-                groupName = "고전 읽는 오후 모임",
-                title = "데미안",
-                coverImageUrl = "https://contents.kyobobook.co.kr/sih/fit-in/400x0/pdt/9791189413408.jpg",
-                authors = "헤르만 헤세",
-                progressRate = 12,
-            ),
-        ),
+        initialUiState = initialUiState,
     )
 }
