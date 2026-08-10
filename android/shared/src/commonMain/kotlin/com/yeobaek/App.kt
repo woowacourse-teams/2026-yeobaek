6ade224d042f3c4d872f5123ebe828f2c6195fd2
Eungyeong Jo <joeungyeong23@gmail.com>
2026-08-10 15:17:47 +0900

    refactor: safeContentPadding 제거 및 Screen 화면에 padding 추가

--- android/shared/src/commonMain/kotlin/com/yeobaek/App.kt
+++ android/shared/src/commonMain/kotlin/com/yeobaek/App.kt
@@ -1,13 +1,6 @@
 package com.yeobaek
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.safeContentPadding
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.yeobaek.core.designsystem.theme.YeobaekTheme
 import com.yeobaek.feature.home.ReaderHomeScreen
@@ -17,18 +10,10 @@ import com.yeobaek.feature.home.rememberReaderHomeStateHolder
 @Preview
 fun App() {
     YeobaekTheme {
-        Column(
-            modifier = Modifier
-                .background(MaterialTheme.colorScheme.primaryContainer)
-                .safeContentPadding()
-                .fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-            val readerHomeStateHolder = rememberReaderHomeStateHolder()
+        val readerHomeStateHolder = rememberReaderHomeStateHolder()
 
-            ReaderHomeScreen(
-                uiState = readerHomeStateHolder.uiState,
-            )
-        }
+        ReaderHomeScreen(
+            uiState = readerHomeStateHolder.uiState,
+        )
     }
 }
--- android/shared/src/commonMain/kotlin/com/yeobaek/feature/home/ReaderHomeScreen.kt
+++ android/shared/src/commonMain/kotlin/com/yeobaek/feature/home/ReaderHomeScreen.kt
@@ -2,8 +2,10 @@ package com.yeobaek.feature.home
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -21,7 +23,9 @@ fun ReaderHomeScreen(
     modifier: Modifier = Modifier,
 ) {
     Column(
-        modifier = modifier.fillMaxWidth(),
+        modifier = modifier
+            .fillMaxSize()
+            .padding(20.dp),
     ) {
         AppTitle(title = "여백")
 
