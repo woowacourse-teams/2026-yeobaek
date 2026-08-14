bf5afb140e41ee53a5742da61682812398625207
Eungyeong Jo <joeungyeong23@gmail.com>
2026-08-14 17:14:44 +0900

    refactor: memberId 수정

--- android/androidApp/src/main/kotlin/com/yeobaek/MainActivity.kt
+++ android/androidApp/src/main/kotlin/com/yeobaek/MainActivity.kt
@@ -29,7 +29,7 @@ class MainActivity : ComponentActivity() {
 fun AppAndroidPreview() {
     App(
         appContainer = AppContainer(
-            memberId = 1
+            memberId = 7
         )
     )
 }
