6f03d3c617f4f9cb319df41dea3d26d054ad14cd
Eungyeong Jo <joeungyeong23@gmail.com>
2026-08-19 20:55:29 +0900

    feat: iOS 슬라이드 뒤로가기 기능 구현

--- android/shared/src/iosMain/kotlin/com/yeobaek/core/platform/PlatformBackHandler.ios.kt
+++ android/shared/src/iosMain/kotlin/com/yeobaek/core/platform/PlatformBackHandler.ios.kt
@@ -1,9 +1,18 @@
 package com.yeobaek.core.platform
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.backhandler.BackHandler
 
+@Suppress("DEPRECATION")
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 internal actual fun PlatformBackHandler(
     enabled: Boolean,
     onBack: () -> Unit,
-) = Unit
+) {
+    BackHandler(
+        enabled = enabled,
+        onBack = onBack,
+    )
+}
