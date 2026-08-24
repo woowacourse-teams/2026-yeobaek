a3902aa0ba42febce6f325deea550ca7611d0147
Eungyeong Jo <joeungyeong23@gmail.com>
2026-08-24 12:56:42 +0900

    refactor: 수정/삭제 메뉴가 케밥 버튼 아래에 나타나도록 수정

--- android/shared/src/commonMain/kotlin/com/yeobaek/feature/reader/component/CommentItem.kt
+++ android/shared/src/commonMain/kotlin/com/yeobaek/feature/reader/component/CommentItem.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
@@ -130,6 +131,7 @@ fun CommentItem(
                 isActionMenuExpanded = false
                 onDelete()
             },
+            modifier = Modifier.padding(top = 32.dp),
         )
     }
 }
